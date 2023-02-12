825856f956c732b7979aa8518415e4e444c49f2c
Anjali s <savantanjali0@gmail.com>
2023-02-12 14:44:54 +0530

    pizza bill temp

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,21 +2,22 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    Pizza p = new Pizza(true);
-    p.addExtraCheese();
-    p.addExtraToppings();
-    p.addTakeaway();
-    System.out.println(p.getBill());
+    DeluxePizza dp = new DeluxePizza(true);
 
-    DeluxePizza dp = new DeluxePizza(false);
-
-   //System.out.println(dp.getPrice());
+    //System.out.println(dp.getPrice());
     //dp.addTakeaway();
 
     dp.addExtraCheese();
     dp.addExtraToppings();
     dp.addTakeaway();
     System.out.println(dp.getBill());
+
+
+    Pizza p = new Pizza(true);
+    p.addExtraCheese();
+    p.addExtraToppings();
+    p.addTakeaway();
+    System.out.println(p.getBill());
 
 
   }
