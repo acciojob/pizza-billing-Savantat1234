5b16c790d705f297f72edf59a6ede6012cc8c3bb
Anjali s <savantanjali0@gmail.com>
2023-02-12 23:40:24 +0530

    pizza bill final soln

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -28,7 +28,7 @@ public class Pizza {
             this.price = 400;
             this.topping = 120;
         }
-        this.bill += "Base price of the pizza: " + this.price + "\n";
+        this.bill += "Base Price Of The Pizza: " + this.price + "\n";
         this.cheese = 80;
     }
 
